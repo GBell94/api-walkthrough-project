622de997b24dd8a55563fe9e295b4bbd18b53b9e
GBell94 <gmb1294@gmail.com>
2022-05-04 10:10:47 +0000

    Add consts to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,7 @@
-_rt4BYRYTN_Q8s51rkS3pzHpUfM
+const API_KEY = "_rt4BYRYTN_Q8s51rkS3pzHpUfM";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+
+
 
